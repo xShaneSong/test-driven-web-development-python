d037bd5057eb2b7fb5b66324ac211f87888feff0
Shane <xShane.Song@live.cn>
2016-03-06 00:01:00 +0800

    use template parameter

--- superlists/functional-tests.py
+++ superlists/functional-tests.py
@@ -10,6 +10,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        #pass
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -29,10 +30,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Bug peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
