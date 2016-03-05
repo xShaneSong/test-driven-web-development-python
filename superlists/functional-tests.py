095b565f3d191a060268a499820a4828691d90db
Shane <xShane.Song@live.cn>
2016-03-05 21:55:31 +0800

    fix chapter3 coding error

--- superlists/functional-tests.py
+++ superlists/functional-tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
