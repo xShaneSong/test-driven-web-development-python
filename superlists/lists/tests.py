2e72aa6e895349dfd33fcb6885479c708247fb9c
Shane <xShane.Song@live.cn>
2016-03-06 15:30:12 +0800

    Redirect after POST, and show all items in templates

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -11,11 +11,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expectd_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expectd_html)
+    #def test_home_page_return_correct_html(self):
+    #    request = HttpRequest()
+    #    response = home_page(request)
+    #    expectd_html = render_to_string('home.html')
+    #    self.assertEqual(response.content.decode(), expectd_html)
         #self.assertTrue(response.content.startswith(b'<html>'))
         #self.assertIn(b'<title>To-Do lists</title>', response.content)
         #self.assertTrue(response.content.endswith(b'</html>'))
@@ -26,9 +26,44 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
-        expectd_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
-        self.assertEqual(response.content.decode(), expectd_html)
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        #self.assertEqual(response.status_code, 302)
+        #self.assertEqual(response['location'], '/')
+        #self.assertIn('A new list item', response.content.decode())
+        #expectd_html = render_to_string(
+        #    'home.html',
+        #    {'new_item_text': 'A new list item'}
+        #)
+        #self.assertEqual(response.content.decode(), expectd_html)
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text = 'itemey 1')
+        Item.objects.create(text = 'itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+        
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
